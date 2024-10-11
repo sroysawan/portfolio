2bbfb87b94e7db109d129e234b1005279d67ff35
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-10-11 16:45:25 +0700

    setting Theme

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,33 @@ function App() {
   const [sectionIds , setSectionIds] = useState([]); //array เปล่า
   const [navbarItems , setNavbarItems] = useState([]); //array เปล่า
   const [currentSection , setCurrentSection] = useState(""); //array เปล่า
+  // สร้าง state สำหรับธีม เริ่มต้นที่ "light"
+  const [theme, setTheme] = useState("lighttheme");
+  // ฟังก์ชันสำหรับสลับธีม
+  const toggleTheme = () => {
+    setTheme(theme === "lighttheme" ? "darktheme" : "lighttheme");
+    // setTheme(theme === "mytheme" ? "darktheme" : "mytheme");
+  };
 
+   // ตั้งค่า data-theme ให้กับ div#root
+   useEffect(() => {
+    document.getElementById("root").setAttribute("data-theme", theme);
+    document.body.setAttribute("data-theme", theme); // เพิ่มบรรทัดนี้
+  }, [theme]); // ทุกครั้งที่ theme เปลี่ยน, อัปเดต data-theme ของ div#root
 
 const addSectionsIds = (sectionId) => {
   // console.log(sectionId)
   const elementId = document.getElementById(sectionId).id;
   setSectionIds((prev) => [...new Set([...prev, elementId])]);
 
-
-const elementTitile = document.getElementById(sectionId).childNodes[0].textContent;
-const obj = {title:elementTitile , sectionId:elementId}
-setNavbarItems((prev) => {
-  if(prev.findIndex(e=>e.title === obj.title) < 0){
-    return [...prev,obj]
-  }
-  return prev;
-})
+  const elementTitile = document.getElementById(sectionId).childNodes[0].textContent;
+  const obj = {title:elementTitile , sectionId:elementId}
+  setNavbarItems((prev) => {
+    if(prev.findIndex(e=>e.title === obj.title) < 0){
+      return [...prev,obj]
+    }
+    return prev;
+  })
 }
 
 
@@ -61,8 +72,8 @@ useEffect(()=> {
 },[sectionIds])
 
   return (
-    <div className="mt-14 mx-auto max-w-6xl grid gap-y-5 lg:grid-cols-[40%_60%]">
-      <LeftSection navbarItems={navbarItems} currentSection={currentSection} />
+    <div data-theme={theme} className="pt-14 mx-auto max-w-6xl grid gap-y-5 lg:grid-cols-[40%_60%]">     
+      <LeftSection navbarItems={navbarItems} currentSection={currentSection} toggleTheme={toggleTheme} theme={theme}/>
       <RightSection onInitial={addSectionsIds}/>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,39 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        primaryTitle: "#127369",
-        primaryContent: "#4C5958",
-        primarySubcontent: "#b5cecb",
-        primaryBase: "#fdfdfd",
-        primaryAccent: "#10403B",
-        primaryBg: "#f6f6f6",
-      },
+      // colors: {
+      //   primaryTitle: "#127369",
+      //   primaryContent: "#4C5958",
+      //   primarySubcontent: "#b5cecb",
+      //   primaryBase: "#fdfdfd",
+      //   primaryAccent: "#10403B",
+      //   primaryBg: "#f6f6f6",
+      //   primaryHover : "#2A323C",
+      // },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    // themes: ["light", "dark"],
+    themes: [
+      {
+        lighttheme: {
+          "primary": "#0F4C75", 
+          "secondary": "#bac4cd",
+          "accent": "#3282B8",
+          "neutral": "#BBE1FA",
+          "base-100": "#f6f6f6",  // พื้นหลังในธีม light
+          "base-content": "#1B262C", // ฟอนต์ในธีม light
+        },
+        darktheme: {
+          "primary": "#34d399",
+          "secondary": "#f472b6",
+          "accent": "#8b5cf6",
+          "neutral": "#191D24",
+          "base-100": "#1F2937", // พื้นหลังในธีม dark
+          "base-content": "#F3F4F6", // ฟอนต์ในธีม dark
+        }
+      },
+    ],
+  },
 }
