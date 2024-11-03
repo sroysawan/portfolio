a68ccb8ac23945c3e4e1d99bf99c18fea7547a18
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-03 15:10:25 +0700

    style: add flex-wrap for component Tech

--- src/components/Tech/index.jsx
+++ src/components/Tech/index.jsx
@@ -1,7 +1,7 @@
 const Tech = ({isHighlight,data}) => {
     return (
         <div>
-            <div className="flex gap-2 text-sm">
+            <div className="flex flex-wrap gap-2 text-sm">
             {
                 data.map((e,i) => (
                     <div key={`${e}-tech-${i}`} className={`bg-neutral px-2 py-1 rounded-md ${isHighlight ? "text-primary cursor-pointer" : ""}`}>{e}</div>
