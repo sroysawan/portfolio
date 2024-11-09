973310c7bf266a213d1776388989a0c45c6b306f
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-09 14:06:18 +0700

    fix: add data descriptions for e-commerce project

--- src/Content/project.js
+++ src/Content/project.js
@@ -11,9 +11,10 @@ export const data = [
             },
         ],
         descriptions:[    
-                // "• Retrieve data from the database and display it on the dashboard",
-                // "• create , read , update and delete tasks on the dashboard",
-                // "• create , read , update and delete employee information",
+            "Admin: Manages users, products, categories, and orders, including updating product information, creating/deleting categories, and changing order statuses",
+            "User: Adds products to a shopping cart, places orders, stores shipping addresses, and manages account settings. Users can also view their order history and manage the cart",
+            "Cloudinary: Used for storing product images securely, reducing the load on your server",
+            "Stripe: Payment gateway for handling transactions in Thai Baht, providing a seamless checkout experience",
         ],
         skills:[
             ["React" , "Tailwaind CSS"],
