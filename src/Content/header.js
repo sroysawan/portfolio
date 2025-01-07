b3a99a71a7522c1e40d2b98cad1a105028dcf671
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-07 21:41:48 +0700

    refactor: text title

--- src/Content/header.js
+++ src/Content/header.js
@@ -2,7 +2,7 @@ import fileName from '../assets/Resume_sroysawan.pdf'
 
 export const data = {
     name: "Sroysawan Kladcheydee",
-    title: "Front End Developer • Looking for entry-level opportunities in software development",
+    title: "Front End Developer • Seeking entry-level roles",
     caption: "sroysawan.kcd@gmail.com",
     btnText: "View Resume",
     link: fileName,
