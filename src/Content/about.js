cb2205747364d22590a6c012df906248a8d035de
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-10 00:00:59 +0700

    change data and file

--- src/Content/about.js
+++ src/Content/about.js
@@ -1,4 +1,4 @@
 export const data = {
     title: "About",
-    description: "My name is Sroysawan Kladcheydee I am 24 years old. I graduated from the Faculty of Computer Engineering at Rajamangala University of Technology Thanyaburi. I have a passion for learning new programming languages and exploring emerging technologies",
+    description: "My name is Sroysawan Kladcheydee I am 25 years old. I graduated from the Faculty of Computer Engineering at Rajamangala University of Technology Thanyaburi. I have a passion for learning new programming languages and exploring emerging technologies",
 }
