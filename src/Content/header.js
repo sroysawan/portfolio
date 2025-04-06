663eeabdb5eddb21413ce0bf028b8152ff60d9ff
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-04-06 17:27:58 +0700

    nothing

--- src/Content/header.js
+++ src/Content/header.js
@@ -2,7 +2,8 @@ import fileName from '../assets/Resume_sroysawan.pdf'
 
 export const data = {
     name: "Sroysawan Kladcheydee",
-    title: "Front End Developer • Seeking entry-level roles",
+    title: "Seeking entry-level roles",
+    // title: "Front End Developer • Seeking entry-level roles",
     caption: "sroysawan.kcd@gmail.com",
     btnText: "View Resume",
     link: fileName,
